0368ba4a7d13df992aebe0b959b106b5766ddb74
AJScott <as243879@falmouth.ac.uk>
2023-02-01 13:16:32 +0000

    Front rig and agent movement on mouse click

--- Assets/Scripts/FootPosition.cs
+++ Assets/Scripts/FootPosition.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class FootPosition : MonoBehaviour
 {
-    public Transform root;
-    public float footSpacing;
-    public float stepDistance;
-    public LayerMask terrainLayer;
+    public LayerMask terrainLayer = default;
+    public Transform root = default;
+    public FootPosition otherFoot = default;
+    public float speed = 1;
+    public float stepDistance = 4;
+    public float stepLength = 4;
+    public float stepHeight = 1;
+    public Vector3 footOffset = default;
+    float footSpacing;
+    Vector3 oldPosition, currentPosition, newPosition;
+    Vector3 oldNormal, currentNormal, newNormal;
+    float lerp;
 
-    Vector3 currentPos;
-    Vector3 newPos;
-
-    private void Update()
+    private void Start()
     {
-        transform.position = currentPos;
+        footSpacing = transform.localPosition.x;
+        currentPosition = newPosition = oldPosition = transform.position;
+        currentNormal = newNormal = oldNormal = transform.up;
+        lerp = 1;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        transform.position = currentPosition;
+        transform.up = currentNormal;
 
         Ray ray = new Ray(root.position + (root.right * footSpacing), Vector3.down);
 
-        if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer))
+        if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
         {
-            if (Vector3.Distance(newPos, info.normal) > stepDistance)
-                newPos = info.point;
+            if (Vector3.Distance(newPosition, info.point) > stepDistance && !otherFoot.IsMoving() && lerp >= 1)
+            {
+                lerp = 0;
+                int direction = root.InverseTransformPoint(info.point).z > root.InverseTransformPoint(newPosition).z ? 1 : -1;
+                newPosition = info.point + (root.forward * stepLength * direction) + footOffset;
+                newNormal = info.normal;
+            }
+        }
+
+        if (lerp < 1)
+        {
+            Vector3 tempPosition = Vector3.Lerp(oldPosition, newPosition, lerp);
+            tempPosition.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+
+            currentPosition = tempPosition;
+            currentNormal = Vector3.Lerp(oldNormal, newNormal, lerp);
+            lerp += Time.deltaTime * speed;
+        }
+        else
+        {
+            oldPosition = newPosition;
+            oldNormal = newNormal;
         }
     }
 
@@ -29,16 +65,23 @@ public class FootPosition : MonoBehaviour
     {
         Ray ray = new Ray(root.position + (root.right * footSpacing), Vector3.down);
 
-        if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer))
+        if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
         {
             Gizmos.color = Color.red;
             Gizmos.DrawLine(root.position + (root.right * footSpacing), info.point);
-            Gizmos.DrawWireSphere(info.point, 0.3f);
         }
         else
         {
             Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(root.position + (root.right * footSpacing), Vector3.down * 100);
+            Gizmos.DrawLine(root.position + (root.right * footSpacing), root.position + (root.right * footSpacing) + (Vector3.down * 5));
         }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(newPosition, 0.1f);
+    }
+
+    public bool IsMoving()
+    {
+        return lerp < 1;
     }
 }
